3ca30ce651505e74e27cbf816adbd72ec7c70524
Maryfellin <maryfelin@gmail.com>
2017-04-20 23:53:13 +0300

    change 20.04.2017

--- Server.cpp
+++ Server.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 enum Packet
 {
-	P_ChatMessage,
-	P_Test
+	Message,
+	Test
 };
 
 char ServerIP[] = "127.0.0.1";
@@ -26,7 +26,7 @@ bool ProcessPacket(int ID, Packet packettype)
 {
 	switch (packettype)
 	{
-	case P_ChatMessage:
+	case Message:
 	{
 		int bufferlength; //Длина строки
 		//для получения строки
@@ -37,17 +37,18 @@ bool ProcessPacket(int ID, Packet packettype)
 		{
 			if (i == ID) //Не отправляйте сообщение чату тому же пользователю, который его отправил
 				continue; //Пропустить пользователя
-			Packet chatmessagepacket = P_ChatMessage; //Создать пакет сообщений чата, который будет отправлен
+			Packet chatmessagepacket = Message; //Создать пакет сообщений чата, который будет отправлен
 			send(Connections[i], (char*)&chatmessagepacket, sizeof(Packet), NULL); //Отправить пакет сообщений чата
 			send(Connections[i], (char*)&bufferlength, sizeof(int), NULL);//Отправить длину буфера клиенту с индексом i
 			send(Connections[i], buffer, bufferlength, NULL);//Отправить сообщение клиенту по индексу i
-			
+			printf("\n");
+
 		}
 		delete[] buffer; // Освободить буфер, чтобы остановить утечку памяти
 		break;
 	}
 	default:
-		printf ("Неизвестный пакет.");
+		printf("Неизвестный пакет.");
 		break;
 	}
 	return true;
@@ -62,8 +63,8 @@ void ClientHandlerThread(int ID)
 		recv(Connections[ID], (char*)&packettype, sizeof(Packet), NULL); //Получение типа пакета от клиента
 
 		//Как только у нас будет тип пакета, обработаем пакет
-		if (!ProcessPacket(ID, packettype)) 
-			break; 
+		if (!ProcessPacket(ID, packettype))
+			break;
 	}
 	closesocket(Connections[ID]); //Закрыть сокет, который использовался для подключения клиента
 }
@@ -76,41 +77,42 @@ int main()
 
 	SOCKADDR_IN addr;
 	int addrlen = sizeof(addr); //Длина адреса (требуется для принятия вызова)
-	addr.sin_addr.s_addr = inet_addr(ServerIP); 
+	addr.sin_addr.s_addr = inet_addr(ServerIP);
 	addr.sin_port = htons(Port); //Port
 	addr.sin_family = AF_INET; //IPv4 сокет
 
 	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // Создаем сокет для прослушивания новых подключений
 	bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // Связывание адреса с сокетом
-	listen(sListen, SOMAXCONN); 
+	listen(sListen, SOMAXCONN);
 
-	printf("Прослушивание входящих соединений...");
+	printf("Прослушивание входящих соединений...\n");
 
 	SOCKET newConnection; //Сокет для подключения клиента
-	int ConnectionCounter = 0; 
+	int ConnectionCounter = 0;
 	for (int i = 0; i < 100; i++)
 
 	{
 		newConnection = accept(sListen, (SOCKADDR*)&addr, &addrlen); //Примите новое подключение
 		if (newConnection == 0) //Если не удалось принять соединение клиента
 		{
-			printf ("Ошибка подключения клиента");
+			printf("Ошибка подключения клиента");
 		}
 		else //Если клиент принят
 		{
-			printf ("Клиент подключился!");
+			printf("Клиент подключился! \n");
 			Connections[i] = newConnection; //Установите сокет в массиве, чтобы быть самым новым соединением, прежде чем создавать поток для обработки сокета этого клиента.
 			ClientCount += 1; //Увеличение общего количества подключенных клиентов
-			cout << "Всего клиентов: " << ClientCount;
+			cout << "Всего клиентов: " << ClientCount << endl;
 			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandlerThread, (LPVOID)(i), NULL, NULL); //Создать поток для обработки этого клиента. Индекс в массиве сокетов для этого потока - это значение (i).
-			std:: string buftest = "Welcome!";
+			std::string buftest = "Добро пожаловать!";
 			int size = buftest.size(); //Получить размер сообщения в байтах и сохранить его в int size
-			Packet chatmessagepacket = P_ChatMessage; //Создать тип пакета: Chat Message, отправляемое на сервер
+			Packet chatmessagepacket = Message; //Создать тип пакета: Chat Message, отправляемое на сервер
 			send(Connections[i], (char*)&chatmessagepacket, sizeof(Packet), NULL); //Отправить тип пакета: Chat Message
 			send(Connections[i], (char*)&size, sizeof(int), NULL); //отправить размер сообщения
 			send(Connections[i], buftest.c_str(), buftest.size(), NULL); //отправить сообщение
+			printf("\n");
 
-			Packet testpacket = P_Test;
+			Packet testpacket = Test;
 			send(Connections[i], (char*)&testpacket, sizeof(Packet), NULL); //отправить тестовый пакет
 		}
 	}
